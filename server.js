efc2741ad4e98aa9f913f01b7327e29b7c4b37c7
Bryan Tran <btran@zr-mac-21.local>
2018-07-25 17:38:38 -0700

    Portforwarding env

--- server.js
+++ server.js
@@ -1,60 +1,61 @@
-const dotenv = require('dotenv').config()
-    , express = require('express')
+const dotenv = require("dotenv").config()
+    , express = require("express")
     , app = express()
-    , crypto = require('crypto')
-    , cookie = require('cookie')
-    , nonce = require('nonce')()
-    , querystring = require('querystring')
+    , crypto = require("crypto")
+    , cookie = require("cookie")
+    , nonce = require("nonce")()
+    , request = require("request-promise")
+    , querystring = require("querystring")
 
 const apiKey = process.env.SHOPIFY_API_KEY
     , apiSecret = process.env.SHOPIFY_API_SECRET
     , scopes = process.env.SHOPIFY_SCOPES
-    , forwardingAddress = "https://89e8b25b.ngrok.io" // Replace this with your HTTPS Forwarding address
+    , forwardingAddress = process.env.SHOPIFY_CALLBACK_URL 
     , PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+    res.send("Hello World!");
 });
 
-app.get('/shopify', (req, res) => {
+app.get("/shopify", (req, res) => {
     const shop = req.query.shop;
     if (shop) {
         const state = nonce();
-        const redirectUri = forwardingAddress + '/shopify/callback';
-        const installUrl = 'https://' + shop +
-            '/admin/oauth/authorize?client_id=' + apiKey +
-            '&scope=' + scopes +
-            '&state=' + state +
-            '&redirect_uri=' + redirectUri;
+        const redirectUri = forwardingAddress + "/shopify/callback";
+        const installUrl = "https://" + shop +
+            "/admin/oauth/authorize?client_id=" + apiKey +
+            "&scope=" + scopes +
+            "&state=" + state +
+            "&redirect_uri=" + redirectUri;
 
-        res.cookie('state', state);
+        res.cookie("state", state);
         res.redirect(installUrl);
     } else {
-        return res.status(400).send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
+        return res.status(400).send("Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request");
     }
 });
 
-app.get('/shopify/callback', (req, res) => {
+app.get("/shopify/callback", (req, res) => {
     const { shop, hmac, code, state } = req.query;
     const stateCookie = cookie.parse(req.headers.cookie).state;
 
     if (state !== stateCookie) {
-        return res.status(403).send('Request origin cannot be verified');
+        return res.status(403).send("Request origin cannot be verified");
     }
 
     if (shop && hmac && code) {
         // DONE: Validate request is from Shopify
         const map = Object.assign({}, req.query);
-        delete map['signature'];
-        delete map['hmac'];
+        delete map["signature"];
+        delete map["hmac"];
         const message = querystring.stringify(map);
-        const providedHmac = Buffer.from(hmac, 'utf-8');
+        const providedHmac = Buffer.from(hmac, "utf-8");
         const generatedHash = Buffer.from(
             crypto
-                .createHmac('sha256', apiSecret)
+                .createHmac("sha256", apiSecret)
                 .update(message)
-                .digest('hex'),
-            'utf-8'
+                .digest("hex"),
+            "utf-8"
         );
         let hashEquals = false;
 
@@ -65,11 +66,11 @@ app.get('/shopify/callback', (req, res) => {
         };
 
         if (!hashEquals) {
-            return res.status(400).send('HMAC validation failed');
+            return res.status(400).send("HMAC validation failed");
         }
 
         // DONE: Exchange temporary code for a permanent access token
-        const accessTokenRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
+        const accessTokenRequestUrl = "https://" + shop + "/admin/oauth/access_token";
         const accessTokenPayload = {
             client_id: apiKey,
             client_secret: apiSecret,
@@ -79,11 +80,11 @@ app.get('/shopify/callback', (req, res) => {
         request.post(accessTokenRequestUrl, { json: accessTokenPayload })
             .then((accessTokenResponse) => {
                 const accessToken = accessTokenResponse.access_token;
-                // DONE: Use access token to make API call to 'shop' endpoint
-                const shopRequestUrl = 'https://' + shop + '/admin/shop.json';
+                // DONE: Use access token to make API call to "shop" endpoint
+                const shopRequestUrl = "https://" + shop + "/admin/shop.json";
                 const shopRequestHeaders = {
                     "Content-Type": "application/json",
-                    'X-Shopify-Access-Token': accessToken,
+                    "X-Shopify-Access-Token": accessToken,
                 };
 
                 request.get(shopRequestUrl, { headers: shopRequestHeaders })
@@ -99,16 +100,16 @@ app.get('/shopify/callback', (req, res) => {
             });
 
     } else {
-        res.status(400).send('Required parameters missing');
+        res.status(400).send("Required parameters missing");
     }
 });
 
-app.get('/check', function (req, res) {
+app.get("/check", function (req, res) {
 
-    const shopRequestUrl = 'https://' + process.env.SHOPIFY_NAME + '/admin/shop.json';
+    const shopRequestUrl = "https://" + process.env.SHOPIFY_NAME + "/admin/shop.json";
     const shopRequestHeaders = {
         "Content-Type": "application/json",
-        'X-Shopify-Access-Token': process.env.SHOPIFY_TOKEN,
+        "X-Shopify-Access-Token": process.env.SHOPIFY_TOKEN,
     };
 
     request.get(shopRequestUrl, { headers: shopRequestHeaders })
@@ -120,17 +121,18 @@ app.get('/check', function (req, res) {
         });
 });
 
-app.get('/play', function (req, res) {
+app.get("/play", function (req, res) {
 
-    const shopRequestUrl = 'https://' + process.env.SHOPIFY_NAME + '/admin/customers.json';
+    const shopRequestUrl = "https://" + process.env.SHOPIFY_NAME + "/admin/customers.json";
     const shopRequestHeaders = {
         "Content-Type": "application/json",
-        'X-Shopify-Access-Token': process.env.SHOPIFY_TOKEN,
+        "X-Shopify-Access-Token": process.env.SHOPIFY_TOKEN,
     };
 
     request.get(shopRequestUrl, { headers: shopRequestHeaders })
         .then((shopResponse) => {
-            res.status(200).end(shopResponse);
+            // res.status(200).end(shopResponse);
+            res.json(JSON.parse(shopResponse));
         })
         .catch((error) => {
             res.status(error.statusCode).send(error.error.error_description);
